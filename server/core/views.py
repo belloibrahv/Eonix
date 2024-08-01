378029196fcbf79b344dda2e1035424c682c7df6
belloibrahv <belloibrahv@gmail.com>
2024-08-01 08:09:32 +0100

    Django REST project setup

--- server/core/views.py
+++ server/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Career
+from .serializers import CareerSerializer
 
-# Create your views here.
+class CareerView(APIView):
+    def get(self, request):
+        careers = Career.objects.all()
+        serializer = CareerSerializer(careers, many=True)
+        return Response(serializer.data)
