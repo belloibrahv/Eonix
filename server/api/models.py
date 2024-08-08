d2f72eb5ef792aeb7bd8c8bd1ab5f0cba224ce30
belloibrahv <belloibrahv@gmail.com>
2024-08-08 10:43:07 +0100

    refined model (to only have the important and used once.)

--- server/api/models.py
+++ server/api/models.py
@@ -21,81 +21,31 @@ class Career(models.Model):
 
     def __str__(self):
         return self.title
+      
+      
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    career_id = models.ForeignKey(Career, on_delete=models.CASCADE, related_name='courses')
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    career = models.ForeignKey(Career, on_delete=models.CASCADE, related_name='courses')
+    
     def __str__(self):
         return self.title
+
 
 class Topic(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='topics')
-    parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subtopics')
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='topics')
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subtopics')
 
     def __str__(self):
         return self.title
-    
-class Resource(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    url = models.URLField(blank=True, null=True)  # Link to external resource
-    name = models.CharField(max_length=255) 
-    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='resources')
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-      
-class Assessment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    topic = models.OneToOneField(Topic, on_delete=models.CASCADE, related_name='assessment')
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.title
-
-class Question(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE, related_name='questions')
-    text = models.TextField()
-    question_type = models.CharField(max_length=50)  # Multiple-choice, True/False, etc.
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.text
-
-class Answer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    text = models.TextField()
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    is_correct = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.text
-
-class Result(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='results')
-    score = models.IntegerField()
-    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE, related_name='results')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.user.email} - {self.assessment.title} - {self.score}'
+  
