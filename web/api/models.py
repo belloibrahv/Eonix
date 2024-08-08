a913dcce3ad5eb4dc80b70293c48c12568a971c3
belloibrahv <belloibrahv@gmail.com>
2024-08-08 17:24:20 +0100

    createD serializers for user registration and login.

--- web/api/models.py
+++ web/api/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(AbstractBaseUser):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+      
+    def create_superuser(self, email, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self.create_user(email, first_name, last_name, **extra_fields)
 
 
-class User(models.Model):
+class User(AbstractBaseUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)  
+    password = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
     def __str__(self):
         return self.email
 
